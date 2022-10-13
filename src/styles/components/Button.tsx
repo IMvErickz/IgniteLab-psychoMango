a40c4d95dd0700510fd3e7131faa1a5e07fc6a7a
IMvErickz <erickspy2003@gmail.com>
2022-10-13 11:55:02 -0300

    fixed bug style button

--- src/styles/components/Button.tsx
+++ src/styles/components/Button.tsx
@@ -11,7 +11,7 @@ export function Button({ children, asChild }: ButtonProps) {
     const Comp = asChild ? Slot : 'button';
     return (
         <Comp className={clsx(
-            'py-4 px-3 bg-amareloMango rounded-2xl font-semibold text-black text-sm w-full transition-colors hover:amareloHover focus:ring-2 ring-white',
+            'py-4 px-3 bg-amarelo-Mango rounded-2xl font-semibold text-black text-sm w-full transition-colors hover:amareloHover focus:ring-2 ring-white',
         )}>{children}</Comp>
     )
 }
